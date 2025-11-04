921be06517b5b67971266b3f7eb4b4dc0f151f74
murphymy04 <137734306+murphymy04@users.noreply.github.com>
2025-11-04 16:40:11 -0600

    Created db schema in mysql

---
+++ db_setup.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE bookstore;
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+USE bookstore;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    isManager BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    rent_price DECIMAL(8,2) NOT NULL,
+    buy_price DECIMAL(8,2) NOT NULL,
+    isRented BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    total_price DECIMAL(10,2) NOT NULL,
+    paid BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE order_books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    purchase_or_rental ENUM('purchase', 'rental') NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (book_id) REFERENCES books(id)
+);
